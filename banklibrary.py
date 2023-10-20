aa8c111a6af29d155bf27af15232ebcaa3e5248c
Nicolasolgoso <145667874+Nicolasolgoso@users.noreply.github.com>
2023-10-20 11:53:27 +0200

    Update banklibrary.py

--- banklibrary.py
+++ banklibrary.py
@@ -2,8 +2,12 @@ from class import BankAccount
 
 account_number = input("¿Cual es el número de tu cuenta?")
 
-account = BankAccount(account-number)
-print(f"Hay{account.get_balance()}€ en tu cuenta")
+operacion = int(input("¿Que operación deseas realizar? \n Para ingresar dinero marca 1: \n Para retirar dinero marca 2: \n Para consultar el saldo marca 3:"))
+
+if operacion == 1:
+    account = BankAccount(account-number)
+    print(f"Hay{account.get_balance()}€ en tu cuenta")
+
 
 funds = float(input("¿Cuánto dinero quieres añadir?"))
 
